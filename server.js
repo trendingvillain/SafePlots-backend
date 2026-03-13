5d00b24c26e4c6e9c0f3a94221bdd379b2df016b
S.Mariselvam <113350913+trendingvillain@users.noreply.github.com>
2026-03-13 09:24:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(cors({
   origin: true,
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 /*------------------------------------------------
